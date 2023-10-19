cfe4cbbfa26a8b54e07b5190bffd0948ea006723
Mukesh Kumar <themukeshmahiya@gmail.com>
2023-10-19 21:58:21 +0530

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
+    base: "/passgenerator",
     plugins: [react()],
-    base: '/',
+    
   }
 
   if (command !== 'serve') {
