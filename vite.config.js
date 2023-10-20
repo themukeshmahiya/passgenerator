14dac0ea8e0d07b86ad24f1e7033f2afd38e0873
Mukesh Kumar <themukeshmahiya@gmail.com>
2023-10-20 15:45:49 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://themukeshmahiya.github.io/passgenerator/",
+  base: "./",
   plugins: [react()],
 })
